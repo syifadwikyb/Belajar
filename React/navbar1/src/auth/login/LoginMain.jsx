35327fd5708b3ca3693a5acfb3bfabedc7677b08
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2025-02-04 23:04:56 +0700

    update

---
+++ React/navbar1/src/auth/login/LoginMain.jsx
@@ -0,0 +1,9 @@
+import ButtonMain from "../../component/ButtonMain";
+
+export default function LoginMain(){
+    return (
+        <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+            <ButtonMain/>
+        </div>
+    )
+}
