48ce9677260ca15dc0e3640b7d02f5fc8d9a51fd
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2025-02-05 22:50:13 +0700

    update

--- React/Percobaan1/src/auth/login/LoginMain.jsx
+++ React/Percobaan1/src/auth/login/LoginMain.jsx
@@ -1,30 +1,35 @@
-import BorderButton from "../../component/BorderButton";
+import BorderButton from "../../component/Button";
 import FormLabel from "../FormLabel";
+import "../../App.css";
+import FooterAuth from "../FooterAuth";
 
-export default function LoginMain() {
+export default function LoginMain({type = "login"}) {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
-      <p className="font-medium text-slate-500 mb-8">
-        Welcome, please enter your detail
-      </p>
-      <form action="">
-        <FormLabel
-          inputName="email"
-          labelName="Email"
-          inputType="text"
-          placeholder="example@gmail.com"
-        />
+    <div className="flex justify-center items-center min-h-screen w-screen">
+      <div className="w-full max-w-xs">
+        <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Welcome, please enter your detail
+        </p>
+        <form action="">
+          <FormLabel
+            inputName="email"
+            labelName="Email"
+            inputType="text"
+            placeholder="example@gmail.com"
+          />
 
-        <FormLabel
-          inputName="password"
-          labelName="Password"
-          inputType="password"
-          placeholder="********"
-        />
-      </form>
+          <FormLabel
+            inputName="password"
+            labelName="Password"
+            inputType="password"
+            placeholder="********"
+          />
+        </form>
 
-      <BorderButton text="Login" />
+        <BorderButton text="Login" />
+        <FooterAuth type={type}/>
+      </div>
     </div>
   );
 }
