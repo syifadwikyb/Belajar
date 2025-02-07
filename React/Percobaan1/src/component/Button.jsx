9738e3c38715e10c7bcb83318356056d52ca3712
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2025-02-07 20:23:37 +0700

    update

--- React/Percobaan1/src/component/Button.jsx
+++ React/Percobaan1/src/component/Button.jsx
@@ -1,7 +1,10 @@
-export default function BorderButton({text}) {
+export default function BorderButton({text, onClick = () => {}, type = "button"}) {
     return(
         <div>
-            <button className="h-10 px-6 font-semibold rounded-md text-white bg-blue-700 w-full" type="submit">
+            <button className="h-10 px-6 font-semibold rounded-md text-white bg-blue-700 w-full" 
+            type={type}
+            onClick={onClick}
+            >
                 {text}
             </button>            
         </div>
--- React/Percobaan1/src/products/ProductMain.jsx
+++ React/Percobaan1/src/products/ProductMain.jsx
@@ -1,4 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import Card from "../component/Card";
+import BorderButton from "../component/Button";
+import "../App.css";
+import Counter from "../component/Counter";
+
 
 export default function ProductMain() {
   const items = [
@@ -14,26 +19,43 @@ export default function ProductMain() {
     },
 
     {
-        id: 2,
-        link1: "#",
-        image: "/img/gambar1.jpeg",
-        link2: "#",
-        name: "Sepatu Baru",
-        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi id facilis molestiae voluptas doloremque, debitis vitae!",
-        price: "Rp.1.000.000",
-        nameInput: "Coba",
-      },
+      id: 2,
+      link1: "#",
+      image: "/img/gambar1.jpeg",
+      link2: "#",
+      name: "Sepatu Baru",
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi id facilis molestiae voluptas doloremque, debitis vitae!",
+      price: "Rp.1.000.000",
+      nameInput: "Coba",
+    },
   ];
 
+  const navigate = useNavigate();
+  const Email = localStorage.getItem("email")
+  const handleLogout = () => {
+    localStorage.removeItem("email")
+    localStorage.removeItem("password")
+    navigate("/login")
+  }
+
   return (
-    <div className="w-screen min-h-full flex justify-center gap-6">
-      {items.map((item) => (
-        <Card>
-          <Card.Header link1={item.link1} image={item.image}/>
-          <Card.Body link2={item.link2} name={item.name} desc={item.desc} />
-          <Card.Footer price={item.price} nameInput={item.nameInput} />
-        </Card>
-      ))}
+    <div className="w-screen ">
+      <div className="flex justify-center h-10 text-white items-center bg-slate-300">
+        {Email}
+        <BorderButton className="ml-5 bg-black" onClick={handleLogout} text="Logout"/>
+      </div>
+      <div className="flex justify-center gap-6">
+        {items.map((item) => (
+          <Card key={item.id}>
+            <Card.Header link1={item.link1} image={item.image} />
+            <Card.Body link2={item.link2} name={item.name} desc={item.desc} />
+            <Card.Footer price={item.price} nameInput={item.nameInput} />
+          </Card>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <Counter/> 
+      </div>
     </div>
   );
 }
