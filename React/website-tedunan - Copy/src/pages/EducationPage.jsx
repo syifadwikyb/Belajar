2a6860ddc216156c3af7911cc8163f3ac9dbc687
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2025-09-09 22:42:47 +0700

    tracking done

--- React/website-tedunan - Copy/src/pages/EducationPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import EducationSection from '../components/education/EducationSection';
-
-const EducationPage = () => {
-  return <EducationSection />;
-};
-
-export default EducationPage;
